07e8762953a20ea828c3bca3d2cbb0b3cad9e55c
amarsanaa.e <amarsanaa.e@itc.gov.mn>
2023-06-16 14:34:11 +0800

    mongol

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
     <main>
       <h1>Home Page</h1>
       <p>
         <Link href="/users">Users</Link>
       </p>
     </main>
+    </>
   )
 }
