51add11bdba2811ca14cae8c55b300b1c34e6687
amarsanaa.e <amarsanaa.e@itc.gov.mn>
2023-06-16 16:07:08 +0800

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,43 +4,20 @@ import { Inter } from 'next/font/google'
 import mg from '../public/images/php.png'
 const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata = {
-//   title: 'amaraa in the house',
-//   description: 'turuu: uuuhuuuuu, amaraa: uuyaa ',
-//   themeColor: 'black',
-//   openGraph: {
-//     title: 'Next.js',
-//     description: 'The React Framework for the Web',
-//     url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FFindingNemoNemo%2F&psig=AOvVaw0QMNM-nw6iBpa__ATc9GIS&ust=1686984185364000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLDAiPiXx_8CFQAAAAAdAAAAABAE',
-//     siteName: 'Next.js',
-//     images: [
-//       {
-//         url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FFindingNemoNemo%2F&psig=AOvVaw0QMNM-nw6iBpa__ATc9GIS&ust=1686984185364000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLDAiPiXx_8CFQAAAAAdAAAAABAE',
-//         width: 800,
-//         height: 600,
-//       },
-//       {
-//         url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FFindingNemoNemo%2F&psig=AOvVaw0QMNM-nw6iBpa__ATc9GIS&ust=1686984185364000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLDAiPiXx_8CFQAAAAAdAAAAABAE',
-//         width: 1800,
-//         height: 1600,
-//         alt: 'My custom alt',
-//       },
-//     ],
-//     locale: 'en_US',
-//     type: 'website',
-//   },
-//   twitter: {
-//     card: 'summary_large_image',
-//     title: 'Ai horvoo',
-//     description: 'olsood bwl ym idchihj bgaarai',
-//     siteId: '1467726470533754880',
-//     creator: '@nextjs',
-//     creatorId: '1467726470533754880',
-//     images: ['https://ibb.co/kDSx379'],
-//   },
-// }
-
-
+export const metadata = {
+  title: 'amaraa in the house',
+  description: 'turuu: uuuhuuuuu, amaraa: uuyaa ',
+  themeColor: 'white',
+  icons: {
+    icon: '/php.png',
+    shortcut: '/php.png',
+    apple: '/php.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-touch-icon-precomposed.png',
+    },
+  },
+}
 
 export default function RootLayout({
   children,
