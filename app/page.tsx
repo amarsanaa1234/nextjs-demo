6d2535339b77174aa3d72ad3e6a0538801301225
amarsanaa.e <amarsanaa.e@itc.gov.mn>
2023-06-16 14:51:20 +0800

    asdf

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
     <main>
-      <h1>Home Page</h1>
+      <h1>Home Page brh</h1>
       <p>
         <Link href="/users">Users</Link>
       </p>
